174879045bf1f6322caee4dd165ba80eae8a875a
Patrick Hund <pahund@team.mobile.de>
2018-01-29 16:18:19 +0100

    work in progress
    Change-Id: I0f4260be8bbfc6e189fff4157169799347954f77

--- packages/demo-app/src/pages/home/index.js
+++ packages/demo-app/src/pages/home/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import hello from '@react-modular-toolkit/demo-module';
 
 const title = 'React Modular Toolkit Demo – Home';
-
 hello();
 
 ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
