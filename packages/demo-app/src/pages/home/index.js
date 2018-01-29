d9194ecc05d6c60c956a2c2b9fd156dd43c62107
Patrick Hund <pahund@team.mobile.de>
2018-01-29 15:43:57 +0100

    importing demo-module to demo-app
    Change-Id: I39dab7d5965eed7def8cee99e6fa6ee730bb6efe

--- packages/demo-app/src/pages/home/index.js
+++ packages/demo-app/src/pages/home/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import hello from '@react-modular-toolkit/demo-module';
 
 const title = 'React Modular Toolkit Demo – Home';
+
+hello();
 
 ReactDOM.render(
     <div>{title}</div>,
