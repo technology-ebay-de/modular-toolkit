dbed8a30cb0350b948407cdd3a5947bea9faeed7
Patrick Hund <pahund@team.mobile.de>
2018-10-16 12:21:27 +0200

    bugfix, bundle build
    Change-Id: I5cc1bd558e8aba516926f4502f2c25762ec0d454

--- packages/demo-module/src/components/HackerNews/HackerNewsContainer.js
+++ packages/demo-module/src/components/HackerNews/HackerNewsContainer.js
@@ -1,5 +1,5 @@
 import { connectSelectors } from '@modular-toolkit/hocs';
-import { compose, withHandlers } from 'recompose';
+import { compose, withHandlers, setDisplayName } from 'recompose';
 
 import { enhanceWithSpinner } from '../../enhancers';
 import { handleLoadingTopStories } from '../../handlers';
@@ -7,10 +7,12 @@ import { selectTopStories } from '../../selectors';
 import HackerNews from './HackerNews';
 
 const enhance = compose(
+    setDisplayName('HackerNewsContainer'),
     connectSelectors({
         topStories: selectTopStories
     }),
     withHandlers({ handleLoadingTopStories }),
+
     enhanceWithSpinner
 );
 
