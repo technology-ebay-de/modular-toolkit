d9c0bd110c7e50dd06a3919a2e23839e56972b35
Patrick Hund <pahund@team.mobile.de>
2018-10-27 21:52:52 +0200

    adde useBrick hook (React 16.7 alpha version feature)
    Change-Id: I5bc1cf563b56d14bf14799b8f32f59fee4d7838c

--- packages/bricks/index-esnext.js
+++ packages/bricks/index-esnext.js
@@ -3,3 +3,4 @@ export { default as combineReducers } from './src/combineReducers';
 export { default as BrickManagerContext } from './src/BrickManagerContext';
 export { default as BrickProvider } from './src/BrickProvider';
 export { default as withBrick } from './src/withBrick';
+export { default as useBrick } from './src/useBrick';
