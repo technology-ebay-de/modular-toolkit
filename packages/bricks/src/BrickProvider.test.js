11f9316bde0d20e38f1c8ac5968bfddc9ad4c837
Patrick Hund <pahund@team.mobile.de>
2018-10-30 16:01:15 +0100

    Unit test for BrickProvider
    Change-Id: I318cf0f8d9bd191084320de9dd3ad9dc03204728

---
+++ packages/bricks/src/BrickProvider.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import BrickProvider from './BrickProvider';
+import { mount } from 'enzyme';
+import BrickManagerContext from './BrickManagerContext';
+
+const sagaMiddleware = jest.fn();
+const store = {};
+const reducer = jest.fn();
+const mockBrickManagerConstructor = jest.fn();
+const consumer = jest.fn();
+jest.mock(
+    './BrickManager',
+    () =>
+        class BrickManager {
+            constructor(...args) {
+                mockBrickManagerConstructor(...args);
+            }
+        }
+);
+
+describe('When I mount a brick provider component', () => {
+    beforeEach(() => {
+        mount(
+            <BrickProvider sagaMiddleware={sagaMiddleware} store={store} reducer={reducer}>
+                <BrickManagerContext.Consumer>{consumer}</BrickManagerContext.Consumer>
+            </BrickProvider>
+        );
+    });
+    describe('a Brick Manager', () =>
+        it('is instantiated with the provided saga middleware, store and reducer', () =>
+            expect(mockBrickManagerConstructor).toBeCalledWith({
+                store,
+                reducer,
+                sagaMiddleware
+            })));
+    describe('the consumer wrapped by the brick provider component', () =>
+        it('has access to the brick manager', () => expect(consumer.mock.calls[0]).toMatchSnapshot()));
+    afterEach(() => jest.clearAllMocks());
+});
