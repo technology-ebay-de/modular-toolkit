de06943f38d5e7e99dda41253ed3b021a39e7c53
Patrick Hund <pahund@team.mobile.de>
2018-10-31 10:08:30 +0100

    updated Wallaby to properly show code coverage
    Change-Id: Ibc269a28f37ab72daaa8ab136c1ff5365025d496

--- wallaby.js
+++ wallaby.js
@@ -5,7 +5,17 @@ const packages = ['hocs', 'selectors', 'bricks'];
 module.exports = wallaby => {
     addPackagesToNodePath(wallaby);
     return {
-        files: ['setupJest.js', 'packages/**/*.js', '!**/*.test.js', '!**/node_modules/**', '!packages/demo-app/**'],
+        files: ['setupJest.js', '!**/*.test.js', '!**/node_modules/**'].concat(
+            packages.map(pkg => `packages/${pkg}/**/*.js`)
+        ),
+
+        filesWithNoCoverageCalculated: [
+            '**/index.js',
+            '**/index-esnext.js',
+            '**/webpack.config.js',
+            '**/__mocks__/**',
+            'setupJest.js'
+        ],
 
         tests: ['**/*.test.js', '!**/node_modules/**'],
 
