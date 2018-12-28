46f5c2f0aa1be367cb8a746132cc461c848be83d
Patrick Hund <pahund@team.mobile.de>
2018-12-28 11:01:16 +0100

    slimmer wallaby config, default value for brick manager context
    Change-Id: I53d77f7eb2004e72013991f7cfa2915714f2d194

--- wallaby.js
+++ wallaby.js
@@ -1,42 +1,29 @@
-const path = require('path');
-
 const packages = ['hocs', 'selectors', 'bricks'];
 
-module.exports = wallaby => {
-    addPackagesToNodePath(wallaby);
-    return {
-        files: ['setupJest.js', '!**/*.test.js', '!**/node_modules/**'].concat(
-            packages.map(pkg => `packages/${pkg}/**/*.js`)
-        ),
+// noinspection JSUnresolvedFunction
+module.exports = wallaby => ({
+    files: ['setupJest.js', '!**/*.test.js', '!**/node_modules/**'].concat(
+        packages.map(pkg => `packages/${pkg}/**/*.js`)
+    ),
 
-        filesWithNoCoverageCalculated: [
-            '**/index.js',
-            '**/index-esnext.js',
-            '**/webpack.config.js',
-            '**/__mocks__/**',
-            'setupJest.js'
-        ],
+    filesWithNoCoverageCalculated: [
+        '**/index.js',
+        '**/index-esnext.js',
+        '**/webpack.config.js',
+        '**/__mocks__/**',
+        'setupJest.js'
+    ],
 
-        tests: ['**/*.test.js', '!**/node_modules/**'],
+    tests: ['**/*.test.js', '!**/node_modules/**'],
 
-        env: {
-            type: 'node',
-            runner: 'node'
-        },
+    env: {
+        type: 'node',
+        runner: 'node'
+    },
 
-        compilers: {
-            '**/*.js': wallaby.compilers.babel()
-        },
+    compilers: {
+        '**/*.js': wallaby.compilers.babel()
+    },
 
-        testFramework: 'jest'
-    };
-};
-
-/* special setup for monorepos: need to add the node_modules directories from all packages to the path */
-function addPackagesToNodePath(wallaby) {
-    const paths = packages.map(pkg => path.join(wallaby.localProjectDir, `packages/${pkg}/node_modules`));
-    if (process.env.NODE_PATH) {
-        paths.unshift(process.env.NODE_PATH);
-    }
-    process.env.NODE_PATH = paths.join(path.delimiter);
-}
+    testFramework: 'jest'
+});
