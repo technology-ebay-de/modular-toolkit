c694406a314b6dcb6d619f237fc840ed910fdff2
Patrick Hund <pahund@team.mobile.de>
2019-01-09 17:09:15 +0100

    Fix for issue #14: original reducer does not overwrite changes from additional reducers anymore
    Change-Id: I8c254c79cce7d640363e07240565847544476fd8

--- packages/bricks/src/utils/mergeReducers.js
+++ packages/bricks/src/utils/mergeReducers.js
@@ -3,7 +3,7 @@ import { mergeObjects, combineNestedReducers } from '.';
 export default (originalReducer, additionalReducers) => {
     const combinedAdditionalReducers = combineNestedReducers(additionalReducers);
     return (state = {}, action = {}) => {
-        const nextStateFromOriginalReducer = originalReducer(state, action);
+        const nextStateFromOriginalReducer = mergeObjects(state, originalReducer(state, action));
         const nextStateFromCombinedAdditionalReducers = combinedAdditionalReducers(
             nextStateFromOriginalReducer,
             action
