ef6b191895aea9841802566dc3c42778badbeb69
Patrick Hund <pahund@ebay.com>
2019-01-09 10:03:04 +0100

    Merge Reducer Fix (#13)
    * Fix for issue #12: additional reducers do not overwrite changes from original reducer anymore
    * fixed security audit issue

--- packages/bricks/src/utils/mergeReducers.js
+++ packages/bricks/src/utils/mergeReducers.js
@@ -4,7 +4,10 @@ export default (originalReducer, additionalReducers) => {
     const combinedAdditionalReducers = combineNestedReducers(additionalReducers);
     return (state = {}, action = {}) => {
         const nextStateFromOriginalReducer = originalReducer(state, action);
-        const nextStateFromCombinedAdditionalReducers = combinedAdditionalReducers(state, action);
+        const nextStateFromCombinedAdditionalReducers = combinedAdditionalReducers(
+            nextStateFromOriginalReducer,
+            action
+        );
         return mergeObjects(nextStateFromOriginalReducer, nextStateFromCombinedAdditionalReducers);
     };
 };
